7f295490a02a92a192a1cc605805f9fb1e0cefde
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-13 19:13:23 +0530

    add socket event to save data to database

--- messageController.ts
+++ messageController.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export const fetchUserDetail = async (data: string) => {
   try {
@@ -12,4 +14,44 @@ export const fetchUserDetail = async (data: string) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+export const saveMessage = async (socket, io, users) => {
+  socket.on("send-msg", async (data) => {
+    const isChatExist = await prisma.conversation.findFirst({
+      where: {
+        OR: [
+          {
+            senderId: data.senderId,
+            receiverId: data.receiverId,
+          },
+          {
+            senderId: data.receiverId,
+            receiverId: data.senderId,
+          },
+        ],
+      },
+    });
+
+    if (!isChatExist) {
+      await prisma.conversation.create({
+        data: {
+          senderId: data.senderId,
+          receiverId: data.receiverId,
+        },
+      });
+    }
+
+    await prisma.message.create({
+      data: {
+        senderId: data.senderId,
+        receiverId: data.receiverId,
+      },
+    });
+    // const sendUserSocket = users.get("9d347d85-672a-488e-8d89-84b9d8d0e043");
+    //   console.log(sendUserSocket);
+    //   if (sendUserSocket) {
+    //     io.to(sendUserSocket).emit("msg-recieve", data.message);
+    //   }
+  });
 };
