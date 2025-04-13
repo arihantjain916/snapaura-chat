afc67bdbee39016cbda6b1ee2ba268c581e25297
arihantjain916 <arihantj916@gmail.com>
2025-04-13 10:12:01 +0530

    feat: replace params type and remove console.log

--- index.ts
+++ index.ts
@@ -68,19 +68,15 @@ const io = new Server(server, {
 
 const onlineUser = new Map();
 io.on("connection", (socket) => {
-  console.log("a user connected");
 
   socket.on("add-user", async (data: string) => {
     const user = await fetchUserDetail(data, client);
     onlineUser.set(user.id, socket.id);
-
-    console.log(onlineUser);
   });
 
   saveMessage(socket, io, onlineUser);
 
   socket.on("disconnect", () => {
     onlineUser.delete(socket.id);
-    console.log("user disconnected");
   });
 });
